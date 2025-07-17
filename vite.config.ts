f5b30f8854b1fb3cc1837e72242a0facd7c6077f
yayakyogi <yayaktaka@gmail.com>
2025-07-17 16:41:10 +0700

    remvoe tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
-import tailwindcss from "@tailwindcss/vite";
+// import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts(), tailwindcss()],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, "./src/main.ts"),

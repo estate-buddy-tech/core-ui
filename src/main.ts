8d8e49e2c3ab1e6aa1a412c8785f2a4dc1feb739
yayakyogi <yayaktaka@gmail.com>
2025-08-26 09:30:59 +0700

    feat: improve provider to check authenticated

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,8 @@
-// Types;
-export * from "./types/user";
+// CSS;
 import "../src/styles/layout.css";
 
-// Client
-export { createIdentiesClient, type IdentiesClientConfig } from "./api/client";
-
-// Hooks
-export {
-  useIdenties,
-  type UseIdentiesConfig,
-  type UseIdentiesReturn,
-} from "./hooks/useIdenties";
-
 // Provider
-export {
-  IdentiesProvider,
-  useIdentiesContext,
-  type IdentiesProviderProps,
-} from "./provider/IdentiesProvider";
+export { CoreUIProvider, useCoreUI } from "./provider/AppProvider";
 
-// Component UI
+// Component
 export { ProfileMenu } from "./components/misc/ProfileMenu/ProfileMenu";
